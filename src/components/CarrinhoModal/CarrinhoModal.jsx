24a19481d724b6670d0018cd41498c77792531d8
DanielRGabriel <danielrgabriellelu@gmail.com>
2024-06-08 14:13:58 -0300

    alterações modal carrinho/ random estrelas

--- src/components/CarrinhoModal/CarrinhoModal.jsx
+++ src/components/CarrinhoModal/CarrinhoModal.jsx
@@ -1,68 +1,73 @@
-import { React, useRef } from 'react'
+import { React, useRef, useContext } from 'react';
 import {
-    Modal,
-    ModalOverlay,
-    ModalContent,
-    ModalHeader,
-    ModalFooter,
-    ModalBody,
-    ModalCloseButton,
-    useDisclosure,
-    Button
-  } from '@chakra-ui/react'
-import { useContext } from 'react';
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure,
+  Button,
+  Flex,
+} from '@chakra-ui/react';
+import { useHistory } from 'react-router-dom';
 import { CarrinhoContext } from '../../context/CarrinhoContext';
-import { useHistory } from 'react-router-dom'
 import ItemCarrinho from '../ItemCarrinho/ItemCarrinho';
 
 function CarrinhoModal() {
-    const { isOpen, onOpen, onClose } = useDisclosure()
-    const history = useHistory()
-    const {carrinho, setCarrinho} = useContext(CarrinhoContext)
-    //usar o set carrinho para atualizar a quantidade de cada produto no pedido
-    const handleComprar = () => {
-      console.log("Comprou!!")
-      history.push("/pedido")
-    }
-    
-    
-    const btnRef = useRef(null)
-    return (
-      <>
-  
-        <Button ref={btnRef} onClick={onOpen}>
-          Carrinho
-        </Button>
-  
-        <Modal
-          onClose={onClose}
-          finalFocusRef={btnRef}
-          isOpen={isOpen}
-          scrollBehavior={'inside'}
-        >
-          <ModalOverlay />
-          <ModalContent>
-            <ModalHeader>Modal Title</ModalHeader>
-            <ModalCloseButton />
-            <ModalBody>
-              
-              {carrinho.map((itemCarrinho, index) => (
-                <ItemCarrinho
-                  key={index}
-                  imgUrl={itemCarrinho.imgUrl}
-                  nome={itemCarrinho.nome}
-                  preco={itemCarrinho.preco}
-                  quantidadePedido={itemCarrinho.quantidadePedido}
-                />
-              ))}
-            </ModalBody>
-            <ModalFooter>
-              <Button onClick={onClose}>Close</Button>
-              <Button onClick={handleComprar}>Finalizar compra</Button>
-            </ModalFooter>
-          </ModalContent>
-        </Modal>
-      </>
-    )
-  }
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const history = useHistory();
+  const { carrinho, setCarrinho } = useContext(CarrinhoContext);
+
+  const handleComprar = () => {
+    console.log('Comprou!!');
+    history.push('/pedido');
+  };
+
+  const btnRef = useRef(null);
+
+  return (
+    <>
+      <Button ref={btnRef} onClick={onOpen}>
+        Carrinho
+      </Button>
+
+      <Modal
+        onClose={onClose}
+        finalFocusRef={btnRef}
+        isOpen={isOpen}
+        scrollBehavior={'inside'}
+      >
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Meu carrinho</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            {carrinho.map((itemCarrinho, index) => (
+              <ItemCarrinho
+                key={index}
+                imgUrl={itemCarrinho.imgUrl}
+                nome={itemCarrinho.nome}
+                preco={itemCarrinho.preco}
+                quantidadePedido={itemCarrinho.quantidadePedido}
+              />
+            ))}
+          </ModalBody>
+          <ModalFooter>
+            <Flex w="100%" justifyContent="flex-end" gap={4}>
+              <Button colorScheme="red" onClick={onClose}>
+                Fechar
+              </Button>
+              <Button colorScheme="blue" onClick={handleComprar}>
+                Finalizar compra
+              </Button>
+            </Flex>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+}
+
 export default CarrinhoModal;
--- src/components/ProdutoCard/ProdutoCard.jsx
+++ src/components/ProdutoCard/ProdutoCard.jsx
@@ -16,18 +16,28 @@ import { useHistory } from 'react-router-dom';
 import { useContext } from 'react';
 import { CarrinhoContext } from '../../context/CarrinhoContext';
 
-function ProdutoCard({id, imgUrl, nome, descricao, preco, categoria, quantidade}) {
-  const history = useHistory()
-  const {carrinho, setCarrinho} = useContext(CarrinhoContext)
+function ProdutoCard({
+  id,
+  imgUrl,
+  nome,
+  descricao,
+  preco,
+  categoria,
+  quantidade,
+}) {
+  const history = useHistory();
+  const gerarEstrelasAleatorias = () => Math.floor(Math.random() * 5) + 1;
+
+  const { carrinho, setCarrinho } = useContext(CarrinhoContext);
   const handleComprar = () => {
-    var itemCarrinho = {id, imgUrl, nome, descricao, preco}
-    setCarrinho([...carrinho, itemCarrinho])
-    console.log(carrinho)
-  }
-  return ( 
+    var itemCarrinho = { id, imgUrl, nome, descricao, preco };
+    setCarrinho([...carrinho, itemCarrinho]);
+    console.log(carrinho);
+  };
+  return (
     <Card maxW="sm">
       <CardBody>
-      <Image
+        <Image
           src={imgUrl}
           alt={descricao}
           borderRadius="lg"
@@ -38,25 +48,31 @@ function ProdutoCard({id, imgUrl, nome, descricao, preco, categoria, quantidade}
         />
         <Stack mt="6" spacing="3">
           <Heading size="md">{nome}</Heading>
-          <Text>
-            {descricao}
-          </Text>
+          <Text>{descricao}</Text>
           <Text color="blue.600" fontSize="2xl">
-           {preco !== undefined ? `R$ ${preco.toFixed(2)}` : 'Preço não disponível'}
+            {preco !== undefined
+              ? `R$ ${preco.toFixed(2)}`
+              : 'Preço não disponível'}
           </Text>
         </Stack>
         <Stack direction="row" spacing={1} justify="center" align="center">
-            {[1, 2, 3, 4, 5].map((_, index) => (
-              <StarIcon key={index} color="yellow.400" w={6} h={6} />
-            ))}
+          {[...Array(gerarEstrelasAleatorias())].map((_, index) => (
+            <StarIcon key={index} color="yellow.400" w={6} h={6} />
+          ))}
         </Stack>
       </CardBody>
       <Divider />
       <CardFooter>
         <ButtonGroup spacing="2">
-          <Button onClick={() => {
-            {history.push(`/produtos/${id}`)}
-          }} variant="solid" colorScheme="blue">
+          <Button
+            onClick={() => {
+              {
+                history.push(`/produtos/${id}`);
+              }
+            }}
+            variant="solid"
+            colorScheme="blue"
+          >
             Saber Mais
           </Button>
           <Button onClick={handleComprar} variant="ghost" colorScheme="blue">
