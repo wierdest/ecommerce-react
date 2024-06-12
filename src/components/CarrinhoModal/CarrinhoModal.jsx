8dce903db956a6485d03cc7098f4cb93aa3fe56d
DanielRGabriel <danielrgabriellelu@gmail.com>
2024-06-11 21:15:51 -0300

    quantidade carrinho editada e tamanho campo

--- src/components/CarrinhoModal/CarrinhoModal.jsx
+++ src/components/CarrinhoModal/CarrinhoModal.jsx
@@ -1,86 +1,80 @@
 import { React, useRef, useContext } from 'react';
 import {
-    Modal,
-    ModalOverlay,
-    ModalContent,
-    ModalHeader,
-    ModalFooter,
-    ModalBody,
-    ModalCloseButton,
-    useDisclosure,
-    Button,
-    Flex
-} from '@chakra-ui/react'
-import { PiShoppingCart } from "react-icons/pi";
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure,
+  Button,
+  Flex,
+} from '@chakra-ui/react';
+import { PiShoppingCart } from 'react-icons/pi';
 import { CarrinhoContext } from '../../context/CarrinhoContext';
 import ItemCarrinho from '../ItemCarrinho/ItemCarrinho';
-import { useHistory } from 'react-router-dom'
+import { useHistory } from 'react-router-dom';
 
 function CarrinhoModal() {
-    const { isOpen, onOpen, onClose } = useDisclosure()
-    const history = useHistory()
-    const {carrinho, setCarrinho} = useContext(CarrinhoContext)
-    //usar o set carrinho para atualizar a quantidade de cada produto no pedido
-    const handleComprar = () => {
-      console.log("Comprou!!")
-      history.push("/pedido")
-    }
-    
-    const btnRef = useRef(null)
-    return (
-      <>
-        <Button ref={btnRef} onClick={onOpen}>
-          Carrinho
-        </Button>
-  
-        <Modal
-          onClose={onClose}
-          finalFocusRef={btnRef}
-          isOpen={isOpen}
-          scrollBehavior={'inside'}
-        >
-          <ModalOverlay />
-          <ModalContent>
-            <ModalHeader>Meu carrinho</ModalHeader>
-            <ModalCloseButton />
-            <ModalBody>
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const history = useHistory();
+  const { carrinho, setCarrinho } = useContext(CarrinhoContext);
+  //usar o set carrinho para atualizar a quantidade de cada produto no pedido
+  const handleComprar = () => {
+    console.log('Comprou!!');
+    history.push('/pedido');
+  };
 
-              {
-                carrinho.length == 0
-                ?
-                <Flex justifyContent="center" alignItems="center" w="100%">
-                  <PiShoppingCart fontSize={64} />
-                </Flex>
-                :
+  const btnRef = useRef(null);
+  return (
+    <>
+      <Button ref={btnRef} onClick={onOpen}>
+        Carrinho
+      </Button>
 
-                carrinho.map((itemCarrinho, index) => (
-                  <ItemCarrinho
-                    key={index}
-                    index={index}
-                    id={itemCarrinho.id}
-                    imgUrl={itemCarrinho.imgUrl}
-                    nome={itemCarrinho.nome}
-                    preco={itemCarrinho.preco}
-                    quantidadePedido={itemCarrinho.quantidadePedido}
-                    quantidadeEstoque={itemCarrinho.quantidadeEstoque}
-                  />
-                ))
-              }
-
-            </ModalBody>
-            <ModalFooter>
+      <Modal
+        size="lg"
+        onClose={onClose}
+        finalFocusRef={btnRef}
+        isOpen={isOpen}
+        scrollBehavior={'inside'}
+      >
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Meu carrinho</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            {carrinho.length == 0 ? (
+              <Flex justifyContent="center" alignItems="center" w="100%">
+                <PiShoppingCart fontSize={64} />
+              </Flex>
+            ) : (
+              carrinho.map((itemCarrinho, index) => (
+                <ItemCarrinho
+                  key={index}
+                  index={index}
+                  id={itemCarrinho.id}
+                  imgUrl={itemCarrinho.imgUrl}
+                  nome={itemCarrinho.nome}
+                  preco={itemCarrinho.preco}
+                  quantidadePedido={itemCarrinho.quantidadePedido}
+                  quantidadeEstoque={itemCarrinho.quantidadeEstoque}
+                />
+              ))
+            )}
+          </ModalBody>
+          <ModalFooter>
             <Flex w="100%" justifyContent="flex-end" gap={4}>
               <Button onClick={onClose}>Fechar</Button>
-              {
-                carrinho.length != 0 && <Button onClick={handleComprar}>Finalizar compra</Button>
-              }
-
+              {carrinho.length != 0 && (
+                <Button onClick={handleComprar}>Finalizar compra</Button>
+              )}
             </Flex>
-              
-            </ModalFooter>
-          </ModalContent>
-        </Modal>
-      </>
-    )
-  }
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+}
 export default CarrinhoModal;
--- src/components/SeletorQuantidade/SeletorQuantidade.jsx
+++ src/components/SeletorQuantidade/SeletorQuantidade.jsx
@@ -1,36 +1,38 @@
-import {
-    Button,
-    HStack,
-    Input,
-    useNumberInput
-  } from '@chakra-ui/react'
+import { Button, HStack, Input, useNumberInput } from '@chakra-ui/react';
 
+function SeletorQuantidade({
+  quantidadeInicial,
+  quantidadeMaxima,
+  setQuantidadePedido,
+}) {
+  const { getInputProps, getIncrementButtonProps, getDecrementButtonProps } =
+    useNumberInput({
+      step: 1,
+      defaultValue: quantidadeInicial,
+      min: 1,
+      max: quantidadeMaxima,
+    });
 
-function SeletorQuantidade( { quantidadeInicial, quantidadeMaxima, setQuantidadePedido }) {
-    const { getInputProps, getIncrementButtonProps, getDecrementButtonProps } =
-      useNumberInput({
-        step: 1,
-        defaultValue: quantidadeInicial,
-        min: 1,
-        max: quantidadeMaxima
-    })
+  const inc = getIncrementButtonProps();
+  const dec = getDecrementButtonProps();
+  const input = getInputProps();
 
-    const inc = getIncrementButtonProps()
-    const dec = getDecrementButtonProps()
-    const input = getInputProps()
+  const alterarQuantidade = (e) => {
+    e.preventDefault();
+    setQuantidadePedido(input.value);
+  };
 
-    const alterarQuantidade = (e) => {
-      e.preventDefault()
-      setQuantidadePedido(input.value)
-    }
-
-    return (
-      <HStack maxW='200px'>
-        <Button onClick={(e) => alterarQuantidade(e)} {...inc}>+</Button>
-        <Input {...input}  />
-        <Button onClick={(e) => alterarQuantidade(e)} {...dec}>-</Button>
-      </HStack>
-    )
+  return (
+    <HStack maxW="200px">
+      <Button onClick={(e) => alterarQuantidade(e)} {...inc}>
+        +
+      </Button>
+      <Input textAlign="center" {...input} />
+      <Button onClick={(e) => alterarQuantidade(e)} {...dec}>
+        -
+      </Button>
+    </HStack>
+  );
 }
 
-export default SeletorQuantidade
+export default SeletorQuantidade;
